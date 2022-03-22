186b2594a9da012b2cbd0bc3d08c832b17c37c5d
jiwannishad <jiwan@arosys.com>
2022-03-22 09:09:49 +0530

    sticky fixed

--- src/app/components/bi-treegrid/directives/sticky.directive.ts
+++ src/app/components/bi-treegrid/directives/sticky.directive.ts
@@ -3,8 +3,10 @@ import {
   AfterViewInit,
   Directive,
   ElementRef,
+  Input,
   NgModule,
   Optional,
+  Renderer2,
 } from '@angular/core';
 
 @Directive({
@@ -22,17 +24,26 @@ export class StickyTableDirective {
   selector: '[sticky]',
 })
 export class StickyDirective implements AfterViewInit {
-  constructor(
-    private el: ElementRef,
-    @Optional() private table: StickyTableDirective
-  ) {}
-
-  ngAfterViewInit() {
-    const el = this.el.nativeElement as HTMLElement;
+  @Input() set sticky(shouldHide: boolean) {
+    if(shouldHide==true){
+      const el = this.el.nativeElement as HTMLElement;
     const { x } = el.getBoundingClientRect();
     el.style.position = 'sticky';
     console.log('this.table.x', this.table.x);
     el.style.left = this.table ? `${x - this.table.x}px` : '0px';
+    }
+//     (shouldHide) ? this.renderer2.setStyle(this.el.nativeElement, 'visibility', 'hidden')
+// : this.renderer2.removeStyle(this.el.nativeElement, 'visibility');
+}
+
+
+  constructor(
+    private el: ElementRef,private renderer2: Renderer2,
+    @Optional() private table: StickyTableDirective
+  ) {}
+
+  ngAfterViewInit() {
+    
   }
 }
 
